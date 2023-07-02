1c76b488d8b0d9767b6686054826cf8efd5df0bf
jasmine cai <caij2082@gmail.com>
2023-07-02 15:15:02 -0400

    fix: audio autoplay + button func

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let music = document.getElementById("music");
 let btn = document.getElementById("music-btn");
 
 btn.onclick = () => {
+    console.log("Audio button clicked!");
     if (music.paused && music.currentTime > 0 && !music.ended) { music.play(); }
     else { music.pause(); }
 };
