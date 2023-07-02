50d13051d7a6a010c12ca9102ffdeb0cc87b45bc
jasmine cai <caij2082@gmail.com>
2023-07-02 15:00:46 -0400

    add: bg music to index

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let music = document.getElementById("music");
+let btn = document.getElementById("music-btn");
+
+btn.onclick = () => {
+    if (music.paused && music.currentTime > 0 && !music.ended) { music.play(); }
+    else { music.pause(); }
+};
