dc7c8781ea3585c732459810ec8523af4646b10d
jasmine cai <caij2082@gmail.com>
2023-07-17 16:09:29 -0400

    add: animations, fix: refactor codebase

--- js/update-index.js
+++ js/update-index.js
@@ -1,15 +1,27 @@
 // BLOG UPDATING
 const blog_list = document.getElementById("blog-list");
 fetch('js/blog-list.json').then(res => res.json()).then(x => {
-    let items = x.map(item => `<li>${item.date} <a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
+    let items = "";
+    if (x.length <= 5) { // only view up to 5 most recent blog posts
+        items = x.reverse().map(item => `<li>${item.date} <a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
+    }
+    else { // if there's more, chop off the rest
+        items = x.slice(-5).reverse().map(item => `<li>${item.date} <a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
+    }
     items += `<li><a href="https://s3gfault.dev/blog/archive" style="text-decoration: none">...</a></li>`;
-    blog_list.innerHTML = `<ul>${items}</ul>`; 
+    blog_list.innerHTML = `${items}`;
 }).catch(err => console.error('Error:', err));
 
 // PROJECT UPDATING
 const projects_list = document.getElementById("projects-list");
 fetch('js/project-list.json').then(res => res.json()).then(x => {
-    let items = x.map(item => `<li><a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
+    let items = "";
+    if (x.length <= 5) {
+        items = x.reverse().map(item => `<li><a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
+    }
+    else {
+        items = x.slice(-5).reverse().map(item => `<li><a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
+    }
     items += `<li><a href="https://s3gfault.dev/projects/archive" style="text-decoration: none">...</a></li>`;
-    projects_list.innerHTML = `<ul>${items}</ul>`; 
+    projects_list.innerHTML = `${items}`; 
 }).catch(err => console.error('Error:', err));
--- webring/site-list.js
+++ webring/site-list.js
@@ -2,5 +2,5 @@ const cool_ppl_list = document.getElementById("cool-ppl-list");
 
 fetch('cool-ppl-webring.json').then(res => res.json()).then(x => {
     const items = x.map(item => `<li><a href="${item.url}">${item.name}</a></li>`).join('');
-        cool_ppl_list.innerHTML = `<ul>${items}</ul>`; 
+        cool_ppl_list.innerHTML = `${items}`; 
 }).catch(err => console.error('Error:', err));
