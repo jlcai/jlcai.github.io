58ed8bbcf742dbdbec35f499cec262ae49477eef
jasmine cai <caij2082@gmail.com>
2023-07-17 14:49:50 -0400

    add: cool ppl web infra

---
+++ webring/site-list.js
@@ -0,0 +1,6 @@
+const cool_ppl_list = document.getElementById("cool-ppl-list");
+
+fetch('cool-ppl-webring.json').then(res => res.json()).then(x => {
+    const items = x.map(item => `<li><a href="${item.url}">${item.name}</a></li>`).join('');
+        cool_ppl_list.innerHTML = `<ul>${items}</ul>`; 
+}).catch(err => console.error('Error:', err));
