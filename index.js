714ee2863a4f0cd15ef4d7a002adcaca21433893
jasmine cai <caij2082@gmail.com>
2023-08-02 12:53:15 -0400

    fix: archive, add: 2023-08-02 miniblogpost

--- index.js
+++
@@ -1,8 +0,0 @@
-let music = document.getElementById("music");
-let btn = document.getElementById("music-btn");
-
-btn.onclick = () => {
-    console.log("Audio button clicked!");
-    if (music.paused && music.currentTime > 0 && !music.ended) { music.play(); }
-    else { music.pause(); }
-};
