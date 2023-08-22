eac1c0f44e7dce1b45c4934012f873970ebd3f8d
jasmine cai <caij2082@gmail.com>
2023-08-22 11:58:28 -0400

    fix: blog redirect

--- js/blog-redirect.js
+++ js/blog-redirect.js
@@ -1,31 +1,32 @@
 const url = window.location.href;
 
 const older = document.getElementById("oldpg");
-const main = document.getElementById("mainpg");
+const home = document.getElementById("mainpg");
 const newer = document.getElementById("newpg");
 
-fetch('../js/blog-list.json').then(res => res.json()).then(x => {
-    let items = [];
-    items = x.map(item => [item.url, item.title]);
+let main_redirect =  `<a href="https://s3gfault.dev/" style="font-weight: bold; text-decoration: none;">Back to Main Page</a>`;
 
-    for (let i = 0; i < items.length; i++) {
+fetch('../js/blog-list.json')
+.then(res => res.json())
+.then(x => {
+    const items = x.map(item => [item.url, item.title]);
+
+    for (let i = 0; i < items.length; ++i) {
         // not oldest, not newest
-        if (items[i][0].includes(url) && (i!==0) && (i!==items.length)) {
+        if (items[i][0].includes(url) && (i !== 0) && (i !== items.length-1)) {
             older.innerHTML = `« <a href="${items[i-1][0]}">${items[i-1][1]}</a>`;
-            main.innerHTML = `<a href="https://s3gfault.dev/" style="font-weight: bold; text-decoration: none;">Back to Main Page</a>`;
+            home.innerHTML = main_redirect;
             newer.innerHTML = `<a href="${items[i+1][0]}">${items[i+1][1]}</a> »`;
         }
         // oldest
-        else if (items[i][0].includes(url) && (i===0)) {
-            older.innerHTML = " ";
-            main.innerHTML = `<a href="https://s3gfault.dev/" style="font-weight: bold; text-decoration: none;">Back to Main Page</a>`;
+        else if (items[i][0].includes(url) && (i === 0)) {
+            home.innerHTML = main_redirect;
             newer.innerHTML = `<a href="${items[i+1][0]}">${items[i+1][1]}</a> »`;
         }
         // newest
-        else {
+        else if (items[i][0].includes(url)){
             older.innerHTML = `« <a href="${items[i-1][0]}">${items[i-1][1]}</a>`;
-            main.innerHTML = `<a href="https://s3gfault.dev/" style="font-weight: bold; text-decoration: none;">Back to Main Page</a>`;
-            newer.innerHTML = ` `;
+            home.innerHTML = main_redirect;
         }
     }
 }).catch(err => console.error('Error:', err));
