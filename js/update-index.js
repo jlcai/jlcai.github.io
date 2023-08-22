34f2a4bba757f90fe6ef132549a7956ea31262f1
jasmine cai <caij2082@gmail.com>
2023-08-22 14:28:28 -0400

    fix: formatting for blog posts

--- js/update-index.js
+++ js/update-index.js
@@ -27,18 +27,18 @@ fetch('js/project-list.json').then(res => res.json()).then(x => {
 }).catch(err => console.error('Error:', err));
 
 
-// PINNED UPATING
-const pinned_list = document.getElementById("pinned-list");
-fetch('js/blog-list.json').then(res => res.json()).then(x => {
-    let items = "";
-    let res = x.reverse().filter(item => item.pinned);
-    items = res.map(item => `<li><a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
-    pinned_list.innerHTML = `${items}`;
-}).catch(err => console.error('Error:', err));
+// // PINNED UPATING
+// const pinned_list = document.getElementById("pinned-list");
+// fetch('js/blog-list.json').then(res => res.json()).then(x => {
+//     let items = "";
+//     let res = x.reverse().filter(item => item.pinned);
+//     items = res.map(item => `<li><a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
+//     pinned_list.innerHTML = `${items}`;
+// }).catch(err => console.error('Error:', err));
 
-fetch('js/project-list.json').then(res => res.json()).then(x => {
-    let items = "";
-    let res = x.reverse().filter(item => item.pinned);
-    items = res.map(item => `<li>[PROJ] <a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
-    pinned_list.innerHTML += `${items}`;
-}).catch(err => console.error('Error:', err));
+// fetch('js/project-list.json').then(res => res.json()).then(x => {
+//     let items = "";
+//     let res = x.reverse().filter(item => item.pinned);
+//     items = res.map(item => `<li>[PROJ] <a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
+//     pinned_list.innerHTML += `${items}`;
+// }).catch(err => console.error('Error:', err));
