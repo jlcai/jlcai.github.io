4089e52a09815fdbf37e8f7a9766f5aecc2f4ada
Jasmine Cai <jlcai@umass.edu>
2024-06-06 04:28:09 -0400

    test 2

--- js/update-index.js
+++ js/update-index.js
@@ -1,31 +1,44 @@
 // BLOG UPDATING
-const blog_list = document.getElementById("blog-list");
-fetch('js/blog-list.json').then(res => res.json()).then(x => {
-    let items = "";
-    if (x.length <= 5) { // only view up to 5 most recent blog posts
-        items = x.reverse().map(item => `<li>${item.date} <a href="${item.url}">${item.title}</a></li>`).join('');
-    }
-    else { // if there's more, chop off the rest
-        items = x.slice(-5).reverse().map(item => `<li>${item.date} <a href="${item.url}">${item.title}</a></li>`).join('');
-    }
-    items += `<li><a href="https://s3gfault.dev/blog/archive" style="text-decoration: none">...</a></li>`;
-    blog_list.innerHTML = `${items}`;
-}).catch(err => console.error('Error:', err));
+// const blog_list = document.getElementById("blog-list");
+// fetch('js/blog-list.json').then(res => res.json()).then(x => {
+//     let items = "";
+//     if (x.length <= 5) { // only view up to 5 most recent blog posts
+//         items = x.reverse().map(item => `<li>${item.date} <a href="${item.url}">${item.title}</a></li>`).join('');
+//     }
+//     else { // if there's more, chop off the rest
+//         items = x.slice(-5).reverse().map(item => `<li>${item.date} <a href="${item.url}">${item.title}</a></li>`).join('');
+//     }
+//     items += `<li><a href="https://s3gfault.dev/blog/archive" style="text-decoration: none">...</a></li>`;
+//     blog_list.innerHTML = `${items}`;
+// }).catch(err => console.error('Error:', err));
 
-// PROJECT UPDATING
-const projects_list = document.getElementById("projects-list");
-fetch('js/project-list.json').then(res => res.json()).then(x => {
+// // PROJECT UPDATING
+// const projects_list = document.getElementById("projects-list");
+// fetch('js/project-list.json').then(res => res.json()).then(x => {
+//     let items = "";
+//     if (x.length <= 5) {
+//         items = x.reverse().map(item => `<li><a href="${item.url}">${item.title}</a></li>`).join('');
+//     }
+//     else {
+//         items = x.slice(-5).reverse().map(item => `<li><a href="${item.url}">${item.title}</a></li>`).join('');
+//     }
+//     items += `<li><a href="https://s3gfault.dev/projects/archive" style="text-decoration: none">...</a></li>`;
+//     projects_list.innerHTML += `${items}`; 
+// }).catch(err => console.error('Error:', err));
+
+// HISTORY LIST
+const history_list = document.getElementById("history-list");
+fetch('js/history-list.json').then(res => res.json()).then(x => {
     let items = "";
-    if (x.length <= 5) {
+    if (x.length <= 20) {
         items = x.reverse().map(item => `<li><a href="${item.url}">${item.title}</a></li>`).join('');
     }
     else {
-        items = x.slice(-5).reverse().map(item => `<li><a href="${item.url}">${item.title}</a></li>`).join('');
+        items = x.slice(-20).reverse().map(item => `<li><a href="${item.url}">${item.title}</a></li>`).join('');
     }
-    items += `<li><a href="https://s3gfault.dev/projects/archive" style="text-decoration: none">...</a></li>`;
+    items += `<li><a href="https://s3gfault.dev/history/archive" style="text-decoration: none">...</a></li>`;
     projects_list.innerHTML += `${items}`; 
 }).catch(err => console.error('Error:', err));
-
 
 // // PINNED UPATING
 // const pinned_list = document.getElementById("pinned-list");
