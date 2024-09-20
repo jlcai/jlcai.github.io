4dbc3d17886d3de602cf7e783d43e78147954def
Jasmine Cai <jlcai@umass.edu>
2024-09-19 20:37:53 -0400

    refactor website, add preview

--- js/update-archive.js
+++ js/update-archive.js
@@ -7,46 +7,46 @@ if (document.getElementById("blog-list")) {
     }).catch(err => console.error('Error:', err));
 }
 
-if (document.getElementById("history-list")) {
-    const history_list = document.getElementById("history-list");
+if (document.getElementById("list_preview")) {
+    const preview_list = document.getElementById("list_preview");
     fetch('../js/history-list.json').then(res => res.json()).then(x => {
         let items = x.reverse().map(item => `<li style="li:before { content: '<b>${item.date}</b>'; padding-right: 7px; }; margin-top:20px; "> <b>${item.date}</b> ${item.long}</li>`).join('');
-        history_list.innerHTML = `${items}`; 
+        preview_list.innerHTML = `${items}`; 
     }).catch(err => console.error('Error:', err));
 }
 
-// UMASS
-// BLOG UPDATING
-if (document.getElementById("umass-amherst-blog-list")) {
-    const blog_list = document.getElementById("umass-amherst-blog-list");
-    fetch('../../js/blog-list.json').then(res => res.json()).then(x => {
-        let items = x.reverse().map(item => {
-            if (item.tags.includes("UMass Amherst")) {
-                console.log(item);
-                return `<li>${item.date} <a href="${item.url}">${item.title}</a> ${item.description}</li>`;
-            }
-        }).join('');
-        console.log(`items: ${items}`)
-        blog_list.innerHTML = `${items}`; 
-    }).catch(err => console.error('Error:', err));
-}
+// // UMASS
+// // BLOG UPDATING
+// if (document.getElementById("umass-amherst-blog-list")) {
+//     const blog_list = document.getElementById("umass-amherst-blog-list");
+//     fetch('../../js/blog-list.json').then(res => res.json()).then(x => {
+//         let items = x.reverse().map(item => {
+//             if (item.tags.includes("UMass Amherst")) {
+//                 console.log(item);
+//                 return `<li>${item.date} <a href="${item.url}">${item.title}</a> ${item.description}</li>`;
+//             }
+//         }).join('');
+//         console.log(`items: ${items}`)
+//         blog_list.innerHTML = `${items}`; 
+//     }).catch(err => console.error('Error:', err));
+// }
 
 
-// EDU
-// BLOG UPDATING
-if (document.getElementById("education-blog-list")) {
-    const blog_list = document.getElementById("education-blog-list");
-    fetch('../../js/blog-list.json').then(res => res.json()).then(x => {
-        let items = x.reverse().map(item => {
-            if (item.tags.includes("education")) {
-                console.log(item);
-                return `<li>${item.date} <a href="${item.url}">${item.title}</a> ${item.description}</li>`;
-            }
-        }).join('');
-        console.log(`items: ${items}`)
-        blog_list.innerHTML = `${items}`; 
-    }).catch(err => console.error('Error:', err));
-}
+// // EDU
+// // BLOG UPDATING
+// if (document.getElementById("education-blog-list")) {
+//     const blog_list = document.getElementById("education-blog-list");
+//     fetch('../../js/blog-list.json').then(res => res.json()).then(x => {
+//         let items = x.reverse().map(item => {
+//             if (item.tags.includes("education")) {
+//                 console.log(item);
+//                 return `<li>${item.date} <a href="${item.url}">${item.title}</a> ${item.description}</li>`;
+//             }
+//         }).join('');
+//         console.log(`items: ${items}`)
+//         blog_list.innerHTML = `${items}`; 
+//     }).catch(err => console.error('Error:', err));
+// }
 
 // PROJECT UPDATING
 if (document.getElementById("projects-list")) {
--- js/update-index.js
+++ js/update-index.js
@@ -1,57 +1,90 @@
-// BLOG UPDATING
-// const blog_list = document.getElementById("blog-list");
-// fetch('js/blog-list.json').then(res => res.json()).then(x => {
-//     let items = "";
-//     if (x.length <= 5) { // only view up to 5 most recent blog posts
-//         items = x.reverse().map(item => `<li>${item.date} <a href="${item.url}">${item.title}</a></li>`).join('');
-//     }
-//     else { // if there's more, chop off the rest
-//         items = x.slice(-5).reverse().map(item => `<li>${item.date} <a href="${item.url}">${item.title}</a></li>`).join('');
-//     }
-//     items += `<li><a href="https://s3gfault.dev/blog/archive" style="text-decoration: none">...</a></li>`;
-//     blog_list.innerHTML = `${items}`;
-// }).catch(err => console.error('Error:', err));
+let preview_list = document.getElementById("list_preview");
+
+const blog_button = document.getElementById("blog_button")
+const project_button = document.getElementById("project_button");
+const research_button = document.getElementById("research_button");
+const history_button = document.getElementById("history_button");
+
+// BLOG BUTTON
+blog_button.addEventListener('click', e => {
+    fetch('js/blog-list.json').then(res => res.json()).then(x => {
+        typingtext.innerHTML = `cd ~/blog && ls -t`;
+        typingtext.style.width = "18ch";
+
+        let items = "";
+        let count = 7;
+        if (x.length <= count) { // only view up to 5 most recent blog posts
+            items = x.reverse().map(item => `<li>${item.date} <a href="${item.url}">${item.title}:</a> ${item.description}</li><br>`).join('');
+        }
+        else { // if there's more, chop off the rest
+            items = x.slice(-count).reverse().map(item => `<li>${item.date} <a href="${item.url}">${item.title}:</a> ${item.description}</li><br>`).join('');
+        }
+        items += `<li><a href="https://s3gfault.dev/blog/archive" style="text-decoration: none">...(more)</a></li>`;
+        preview_list.innerHTML = `${items}`;
+    }).catch(err => console.error('Error:', err));
+});
 
 // // PROJECT UPDATING
-// const projects_list = document.getElementById("projects-list");
-// fetch('js/project-list.json').then(res => res.json()).then(x => {
-//     let items = "";
-//     if (x.length <= 5) {
-//         items = x.reverse().map(item => `<li><a href="${item.url}">${item.title}</a></li>`).join('');
-//     }
-//     else {
-//         items = x.slice(-5).reverse().map(item => `<li><a href="${item.url}">${item.title}</a></li>`).join('');
-//     }
-//     items += `<li><a href="https://s3gfault.dev/projects/archive" style="text-decoration: none">...</a></li>`;
-//     projects_list.innerHTML += `${items}`; 
-// }).catch(err => console.error('Error:', err));
+project_button.addEventListener('click', e => {
+    typingtext.innerHTML = `cd ~/projects && ls -t`;
+    typingtext.style.width = "22ch";
+
+    preview_list.innerHTML = `<li>dont look im not done yet</li>`
+    // fetch('js/project-list.json').then(res => res.json()).then(x => {
+    //     let items = "";
+    //     if (x.length <= 5) {
+    //         items = x.reverse().map(item => `<li><a href="${item.url}">${item.title}</a></li><br>`).join('');
+    //     }
+    //     else {
+    //         items = x.slice(-5).reverse().map(item => `<li><a href="${item.url}">${item.title}</a></li><br>`).join('');
+    //     }
+    //     items += `<li><a href="https://s3gfault.dev/projects/archive" style="text-decoration: none">...(more)</a></li>`;
+    //     preview_list.innerHTML = `${items}`; 
+    // }).catch(err => console.error('Error:', err));
+});
+
+// // PROJECT UPDATING
+research_button.addEventListener('click', e => {
+    typingtext.innerHTML = `cd ~/research && ls -t`;
+    typingtext.style.width = "22ch";
+
+    preview_list.innerHTML = `<li>dont look im not done yet</li>`
+    // fetch('js/project-list.json').then(res => res.json()).then(x => {
+    //     let items = "";
+    //     if (x.length <= 5) {
+    //         items = x.reverse().map(item => `<li><a href="${item.url}">${item.title}</a></li><br>`).join('');
+    //     }
+    //     else {
+    //         items = x.slice(-5).reverse().map(item => `<li><a href="${item.url}">${item.title}</a></li><br>`).join('');
+    //     }
+    //     items += `<li><a href="https://s3gfault.dev/projects/archive" style="text-decoration: none">...(more)</a></li>`;
+    //     preview_list.innerHTML = `${items}`; 
+    // }).catch(err => console.error('Error:', err));
+});
 
 // HISTORY LIST
-const history_list = document.getElementById("history-list");
-fetch('js/history-list.json').then(res => res.json()).then(x => {
-    let items = "";
-    if (x.length <= 20) {
-        items = x.reverse().map(item => `<li>${item.date} ${item.short}</li>`).join('');
-    }
-    else {
-        items = x.slice(-20).reverse().map(item => `<li>${item.date} ${item.short}</li>`).join('');
-    }
-    items += `<li><a href="https://s3gfault.dev/history/archive" style="text-decoration: none">...</a></li>`;
-    history_list.innerHTML += `${items}`; 
-}).catch(err => console.error('Error:', err));
+let history_default = e => {
+    fetch('js/history-list.json').then(res => res.json()).then(x => {
+        typingtext.innerHTML = `cd ~/history && ls -t`;
+        typingtext.style.width = "21ch";
 
-// // PINNED UPATING
-// const pinned_list = document.getElementById("pinned-list");
-// fetch('js/blog-list.json').then(res => res.json()).then(x => {
-//     let items = "";
-//     let res = x.reverse().filter(item => item.pinned);
-//     items = res.map(item => `<li><a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
-//     pinned_list.innerHTML = `${items}`;
-// }).catch(err => console.error('Error:', err));
+        let items = "";
+        let count = 6;
+        if (x.length <= count) {
+            items = x.reverse().map(item => `<li>${item.date} ${item.long}</li><br>`).join('');
+        }
+        else {
+            items = x.slice(-count).reverse().map(item => `<li>${item.date} ${item.long}</li><br>`).join('');
+        }
+        items += `<li><a href="https://s3gfault.dev/history/archive" style="text-decoration: none">...(more)</a></li>`;
+        preview_list.innerHTML = `${items}`; 
+    }).catch(err => console.error('Error:', err));
+};
 
-// fetch('js/project-list.json').then(res => res.json()).then(x => {
-//     let items = "";
-//     let res = x.reverse().filter(item => item.pinned);
-//     items = res.map(item => `<li>[PROJ] <a href="${item.url}">${item.title}</a> ${item.description}</li>`).join('');
-//     pinned_list.innerHTML += `${items}`;
-// }).catch(err => console.error('Error:', err));
+history_button.addEventListener('click', history_default, false);
+window.addEventListener("load", function() {
+    history_default();
+});
+
+// TYPING ANIAMTION THING
+let typingtext = document.getElementById("typing-demo-3");
